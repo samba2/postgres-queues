b43e850ea0a3823b9b59ff7109573abc1537b0a5
Maik Toepfer <maik.toepfer@bmw.de>
2019-12-08 00:40:05 +0100

    make and drop functions

--- queue/queue_reader.py
+++ queue/queue_reader.py
@@ -6,17 +6,24 @@ conn = psycopg2.connect("dbname=postgres user=samba")
 conn.autocommit = True
 cursor = conn.cursor()
 
+cursor.execute("SELECT drop_queue('orderdata')")
+# create table + trigger
+cursor.execute("SELECT make_queue('orderdata')")
+
+# some test data
+cursor.execute("INSERT INTO orderdata VALUES ('order1')")
+cursor.execute("INSERT INTO orderdata VALUES ('order2')")
+
 # prod code
 def main():
     while True:
-        entry = read_queue_plpgsql("queue", cursor)
+        entry = read_queue("orderdata", cursor)
         print("Received: " + entry)
-
 
 # library code
 #
 # TODO make more robust (ignore events without value)
-def read_queue_plpgsql(queue_name, cursor):
+def read_queue(queue_name, cursor):
     def read_single_queue_entry():
         cursor.execute(f"SELECT read_queue_entry('{queue_name}')")
         entry = cursor.fetchone() 
@@ -25,7 +32,7 @@ def read_queue_plpgsql(queue_name, cursor):
         else:
             return None
 
-    cursor().execute(f"LISTEN {queue_name};")
+    cursor.execute(f"LISTEN {queue_name};")
     entry = read_single_queue_entry()        
     if entry:
         return entry
@@ -36,41 +43,6 @@ def read_queue_plpgsql(queue_name, cursor):
         while conn.notifies:
             notify = conn.notifies.pop(0)
             return read_single_queue_entry()
-
-
-def read_queue_all_python():
-    entry=read_queue_entry()
-    if entry:
-        return entry
-
-    if select.select([conn],[],[],60) == ([],[],[]):
-        print("DEBUG Timeout")
-    else:
-        conn.poll()
-        while conn.notifies:
-            # pop is important to consume event
-            notify = conn.notifies.pop(0)
-            # print("DEBUG: Got NOTIFY:", notify.pid, notify.channel, notify.payload)
-            return read_queue_entry()
-
-def read_queue_entry():
-    cursor.execute("""
-    DELETE FROM queue
-    WHERE id = (
-    SELECT id
-    FROM queue
-    ORDER BY id
-    FOR UPDATE SKIP LOCKED
-    LIMIT 1
-    )
-    RETURNING *;
-    """)
-
-    entry=cursor.fetchone()
-    if entry is None:
-        return None
-    else:
-        return entry[0]
 
 if __name__ == "__main__":
     main()
--- queue/setup-queue.sql
+++ queue/setup-queue.sql
@@ -1,43 +1,61 @@
-DROP TABLE IF EXISTS queue;
-CREATE TABLE queue (
-    value       text,
-    id          bigserial PRIMARY KEY
-);
-
--- trigger function
--- TODO make generic
-CREATE OR REPLACE FUNCTION queue_notify() RETURNS trigger AS
+CREATE OR REPLACE FUNCTION make_queue(queue_name text) RETURNS void AS
 $BODY$
 BEGIN
-    PERFORM pg_notify('queue', '');
+    EXECUTE format('
+        CREATE TABLE %s (  
+        value       text, 
+        id          bigserial PRIMARY KEY)'
+        , queue_name);
+
+    EXECUTE format('
+        CREATE TRIGGER %s_trigger
+        AFTER INSERT
+        ON %s
+        FOR EACH ROW
+        EXECUTE PROCEDURE generic_queue_notify()'
+        , queue_name, queue_name);
+
+    RAISE NOTICE 'Queue "queue_name" was successfully created.';
+END;
+$BODY$
+LANGUAGE 'plpgsql';
+
+
+-- used by queue tables to perform the notification
+CREATE OR REPLACE FUNCTION generic_queue_notify() RETURNS trigger AS
+$BODY$
+BEGIN
+    PERFORM pg_notify(TG_TABLE_NAME, '');
     RETURN new;
 END;
 $BODY$
 LANGUAGE 'plpgsql';
 
--- trigger requires trigger function
-DROP TRIGGER IF EXISTS queue_trigger ON queue;
-CREATE TRIGGER queue_trigger
-AFTER INSERT
-ON queue
-FOR EACH ROW
-EXECUTE PROCEDURE queue_notify();
+
+CREATE OR REPLACE FUNCTION drop_queue(queue_name text) RETURNS void AS
+$BODY$
+BEGIN
+    EXECUTE format('DROP TABLE IF EXISTS %s', queue_name);
+    RAISE NOTICE 'Queue "queue_name" has been removed.';
+END;
+$BODY$
+LANGUAGE 'plpgsql';
+
 
 CREATE OR REPLACE FUNCTION read_queue_entry(queue_name text) RETURNS SETOF text AS
 $BODY$
-DECLARE
-    QUEUE_READ_STATEMENT constant text := 
-        'DELETE FROM %s '
-        'WHERE id = ( '
-            'SELECT id '
-            'FROM %s '
-            'ORDER BY id '
-            'FOR UPDATE SKIP LOCKED '
-            'LIMIT 1 '
-        ') '
-        'RETURNING value';
 BEGIN
-    RETURN QUERY EXECUTE format(QUEUE_READ_STATEMENT, queue_name, queue_name);
+    RETURN QUERY EXECUTE format('
+        DELETE FROM %s 
+        WHERE id = ( 
+            SELECT id 
+            FROM %s 
+            ORDER BY id 
+            FOR UPDATE SKIP LOCKED 
+            LIMIT 1 
+        ) 
+        RETURNING value'
+    , queue_name, queue_name);
 END;
 $BODY$
 LANGUAGE 'plpgsql';
