9da8cf5a84394b9664f6f865efa5218c6b23953d
Maik Toepfer <maik.toepfer@bmw.de>
2019-12-11 14:41:30 +0100

    message processing per transaction

--- log/log_example.py
+++ log/log_example.py
@@ -3,53 +3,52 @@ import psycopg2
 import psycopg2.extensions
 import select
 
-conn = psycopg2.connect("dbname=postgres user=samba")
+message_log_connection = psycopg2.connect("dbname=postgres user=samba")
 
 # prod code
 def main():
-    while True:
-        # TODO move transaction to outside
-        # https://github.com/malthe/pq/blob/b3381fee2f1a683c4178fd694a9c32af3aec5f55/pq/__init__.py#L182
-        entry = read_log("orderdata", conn)
-        print("Received: " + entry)
+    read_messages_forever(
+        log_name="orderdata", 
+        connection=message_log_connection, 
+        per_message_callback=print_message)
 
-# # library code
-def read_log(log_name, conn):
-    with transaction(conn) as cursor:
+def print_message(received_message):
+    print("Received: " + received_message)
+
+
+# library code
+def read_messages_forever(log_name, connection, per_message_callback):
+
+    @contextmanager
+    def transaction(conn,):
+        cursor = conn.cursor()
+        try:
+            yield cursor
+            conn.commit()
+        except:
+            conn.rollback()
+            raise
+        finally:
+            cursor.close()
+
+    with transaction(connection) as cursor:
         cursor.execute(f"LISTEN log_{log_name};")
+
     while True:
         # read as long as there are DB entries in the queue        
-        with transaction(conn) as cursor:
+        with transaction(connection) as cursor:
             cursor.execute(f"SELECT read_log_entry('{log_name}')")
             entry = cursor.fetchone() 
             if entry:
-                return entry[0]
+                per_message_callback(entry[0])
+                continue
 
-            # block until event is received or timeout happens (100 years)
-        select.select([conn],[],[], 3153600000) == ([],[],[])
-        conn.poll()
+        # block until event is received or timeout happens (100 years)
+        select.select([connection],[],[], 3153600000) == ([],[],[])
+        connection.poll()
         # "eat" all notifications
-        conn.notifies[:] = []
+        connection.notifies[:] = []
 
-@contextmanager
-def transaction(conn, **kwargs):
-    """Context manager.
-    Execute statements within a transaction.
-    >>> with transaction(conn) as cursor:
-    ...     cursor.execute(...)
-    ...     return cursor.fetchall()
-    """
-
-    cursor = conn.cursor(**kwargs)
-
-    try:
-        yield cursor
-        conn.commit()
-    except:
-        conn.rollback()
-        raise
-    finally:
-        cursor.close()
 
 if __name__ == "__main__":
     main()
