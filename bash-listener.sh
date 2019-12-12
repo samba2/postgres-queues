70c0eed848bcdc47e54c99dd9459ce1fbf6835d2
Maik Toepfer <maik.toepfer@bmw.de>
2019-12-12 10:47:31 +0100

    refactoring

--- bash-listener.sh
+++ bash-listener.sh
@@ -3,78 +3,84 @@
 set -uo pipefail
 #set -x
 
-trap "_ctrl_c" INT
+### Library ###
+function read_messages_forever() (
+  trap "cleanup" INT
 
-function read_messages_forever() {
-  local db_uri=$1
-  local log_name=$2
-  local per_message_callback=$3
-  local message=
+  local g_log_name=$1
+  local g_db_uri=$2
+  local g_per_message_callback=$3
+  
+  local g_poll_loop_pid=
+  local g_psql_input=
+  local g_psql_output=
 
-  _setup_poll "${db_uri}" "${log_name}"
-  # read existing once
-  _read_log_entries_from_db "${db_uri}" "${log_name}" "${per_message_callback}"
-  while read line; do
-    if echo "${line}" | grep -q "Asynchronous notification \"log_${log_name}\" received"; then
-      _read_log_entries_from_db "${db_uri}" "${log_name}" "${per_message_callback}"
-    fi
-  done < ${output}
-}
+  # started at the bottom
+  function run_poll_loop() {
+    # read existing once
+    _read_log_entries_from_db
+    _poll_psql
+    while read line; do
+      if echo "${line}" | \
+         grep -q "Asynchronous notification \"log_${g_log_name}\" received"; then
+        _read_log_entries_from_db
+      fi
+    done < ${g_psql_output}
+  }
 
-function _setup_poll() {
-  local db_uri=$1
-  local log_name=$2
+  function _read_log_entries_from_db() {
+    local message=
 
-  # setup polled psql in background
-  input=$(mktemp -t --dry-run psql-input.XXXX)
-  output=$(mktemp -t --dry-run psql-output.XXXX)
-  mkfifo ${input} ${output}
-  psql "${db_uri}" < ${input}  2>&1 > ${output} &
-  psql_pid=$!
-  exec 3>${input}
-  _poll_loop "${log_name}" &
-  poll_loop_pid=$!
-}
+    while true; do
+      message=$(echo "SELECT read_log_entry('${g_log_name}')" | \
+                psql --quiet --tuples-only --no-align --no-psqlrc "${g_db_uri}")
+      if [ "${message}" ]; then
+        ${g_per_message_callback} "${message}"
+      else
+        break
+      fi
+    done
+  }
 
-function _poll_loop() {
-  local log_name=$1
+  function _poll_psql() {
+    _setup_named_pipes
+    psql --no-psqlrc "${g_db_uri}" < ${g_psql_input}  2>&1 > ${g_psql_output} &
+    exec 3>${g_psql_input} # keep input open
+    _poll_loop &
+    g_poll_loop_pid=$!
+  }
 
-  echo "LISTEN log_${log_name};" >&3
-  while true; do
-    echo "SELECT 1;" >&3
-    sleep 1
-  done
-}
+  function _setup_named_pipes() {
+    g_psql_input=$(mktemp -t --dry-run psql-psql_input.XXXX)
+    g_psql_output=$(mktemp -t --dry-run psql-psql_output.XXXX)
+    mkfifo ${g_psql_input} ${g_psql_output}
+  }
+  
+  function _poll_loop() {
+    echo "LISTEN log_${g_log_name};" > ${g_psql_input}
+    while true; do
+      echo "SELECT 1;" > ${g_psql_input}
+      sleep 1
+    done
+  }
 
-function _read_log_entries_from_db() {
-  local db_uri=$1
-  local log_name=$2
-  local per_message_callback=$3
-  local message=
+  function cleanup() {
+    echo "Exiting"
+    kill ${g_poll_loop_pid} 2>/dev/null
+    echo "end; \quit" > ${g_psql_input}
+    rm -f ${g_psql_input}
+    rm -f ${g_psql_output}
+    exit
+  }
 
-  while true; do
-    message=$(echo "SELECT read_log_entry('${log_name}')" | psql -qtAX "${db_uri}")
-    if [ "${message}" ]; then
-      ${per_message_callback} "${message}"
-    else
-      break
-    fi
-  done
-}
-
-function _ctrl_c() {
-  echo "Exiting"
-  kill ${poll_loop_pid}
-  kill ${psql_pid}
-  rm -f ${input}
-  rm -f ${output}
-  exit
-}
+  run_poll_loop
+)
 
 
+######   Production code #########
 function print_message() {
   local message=$1
   echo "Received: ${message}"
 }
 
-read_messages_forever "postgres://samba@/postgres" "orderdata" "print_message"
+read_messages_forever "orderdata" "postgres://samba@/postgres" "print_message"
